de0037dc9d60e0e23242381f5d41d26e96d147eb
ibbi <hi@ibbi.io>
2019-05-03 19:55:27 -0700

    display score

--- mainApp/App.js
+++ mainApp/App.js
@@ -8,13 +8,7 @@
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-	android:
-		'Double tap R on your keyboard to reload,\n' +
-		'Shake or press menu button for dev menu',
-});
+import ScoreDial from './components/ScoreDial';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -30,9 +24,7 @@ export default class App extends Component<Props> {
 		console.log(this.state.score);
 		return (
 			<View style={styles.container}>
-				<Text style={styles.welcome}>Welcome to React Native!</Text>
-				<Text style={styles.instructions}>To get started, edit App.js</Text>
-				<Text style={styles.instructions}>{instructions}</Text>
+				<ScoreDial score={this.state.score} />
 			</View>
 		);
 	}
---
+++ mainApp/components/ScoreDial.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+const ScoreDial = (props) => {
+	return <Text>{props.score}</Text>;
+};
+
+export default ScoreDial;
