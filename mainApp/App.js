2cc666ef671ce82e51852362f73958a7bbd93e7e
ibbi <hi@ibbi.io>
2019-05-09 17:21:50 -0700

    broke camera, added text rec

--- mainApp/App.js
+++ mainApp/App.js
@@ -9,7 +9,7 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import ScoreDial from './components/ScoreDial';
-import Camera from './components/Camera';
+import Camera from './components/Camera1';
 
 type Props = {};
 export default class App extends Component<Props> {
