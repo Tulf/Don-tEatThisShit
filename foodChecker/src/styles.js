23201124744a930b0152743d2b88e5f8f23d7405
ibbi <hi@ibbi.io>
2019-05-09 18:00:06 -0700

    wow screw this

---
+++ foodChecker/src/styles.js
@@ -0,0 +1,57 @@
+import { StyleSheet } from 'react-native';
+import Dimensions from './Dimensions';
+export const screenHeight = Dimensions.screenHeight;
+export const screenWidth = Dimensions.screenWidth;
+const styles = StyleSheet.create({
+	screen: {
+		backgroundColor: '#000000',
+		flex: 1,
+	},
+	camera: {
+		position: 'absolute',
+		width: Dimensions.screenWidth,
+		height: Dimensions.screenHeight,
+		alignItems: 'center',
+		justifyContent: 'center',
+		top: 0,
+		left: 0,
+		flex: 1,
+	},
+	imageBackground: {
+		position: 'absolute',
+		width: Dimensions.screenWidth,
+		height: Dimensions.screenHeight,
+		alignItems: 'flex-start',
+		justifyContent: 'flex-start',
+		top: 0,
+		left: 0,
+	},
+	buttonContainer: {
+		width: 70,
+		height: 70,
+		backgroundColor: '#ffffff',
+		borderRadius: 35,
+		position: 'absolute',
+		bottom: 36,
+		alignSelf: 'center',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	button: {
+		width: 64,
+		height: 64,
+		backgroundColor: '#ffffff',
+		borderRadius: 32,
+		borderWidth: 4,
+		borderColor: '#000000',
+	},
+	boundingRect: {
+		position: 'absolute',
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderWidth: 2,
+		borderColor: '#FF6600',
+	},
+});
+
+export default styles;
